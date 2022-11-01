c882368a2c29b0ad14b031287c2efe0820120dc1
semenkulikov <saifutdinov2006@mail.ru>
2022-11-01 18:41:50 +0300

    working on the lowprice command

--- states/low_price_info.py
+++ states/low_price_info.py
@@ -3,6 +3,9 @@ from telebot.handler_backends import State, StatesGroup
 
 class HotelLowPriceState(StatesGroup):
     city = State()
+    check_city = State()
+    check_in = State()
+    check_out = State()
     count_hotel = State()
     photo_upload = State()
     count_photos = State()
